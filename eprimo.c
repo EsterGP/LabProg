f7194f791c88091065b865d4f08dad5bb1c0c56e
Ester G. Pais <estergpais@hotmail.com>
2018-11-13 15:26:38 -0200

    Update eprimo.c

--- eprimo.c
+++ eprimo.c
@@ -2,22 +2,28 @@
 #include <math.h>
 #define sim 1
 #define nao 0
+#define N 1000
 
-int primo(int num){
+int primo(int num, int primos[]){
 	int i, eprimo = sim;
-	for(i=2;i<=(sqrt(num)) && eprimo==sim;i++){
-		if((num%i) == 0) eprimo = nao;
-	}
+	for(i=0;primos[i]<=(sqrt(num)) && eprimo==sim;i++)
+		if((num%primos[i]) == 0) eprimo = nao;
 	
 	if(eprimo == sim){
 		printf("%d\t", num);
 		return sim;
 	}
-	
 	return nao;
 }
 
-void main(){
-	int i, nprimo,eprimo = nao;
-	for(i=2;nprimo < 1000;i++) if((eprimo = primo(i)) == sim) ++nprimo;
+int main(){
+	int i, eprimo = nao, primos[N];
+	int nprimo = 0;
+	
+	primos[0] = 2;
+	printf("%d\t", primos[0]);
+	
+	for(i=3;nprimo < N-1;i++)
+		if((eprimo = primo(i,primos)) == sim)	primos[++nprimo] = i;
+	return 0;
 }
