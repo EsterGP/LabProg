573ca77c352e9ad5d740fe0bd6b6e1d2fdca6809
Ester G. Pais <estergpais@hotmail.com>
2018-11-29 16:30:09 -0200

    Update P1.c

--- P1.c
+++ P1.c
@@ -1,23 +1,28 @@
 #include <stdio.h>
+#define AVANCA 1
+#define RECUA 0
 
 void leTexto(char vetor[]);
 void escreve(char vetor[]);
-void maiuscula(char vetor[]);
-void minuscula(char vetor[]);
+void criptografa(char vetor[], int vet_num[]);
+void maiuscula(char vetor[], int c);
+void minuscula(char vetor[], int c);
 void avancar();
 void recuar();
 
 int main(){
 	char textoIn[3000];
-	leTexto(textoIn);
+	int vet_num[12]; //porque a matricula tem 12 dígitos
+	leTexto(textoIn, 3000);
+	criptografa(textoIn, vet_num);
 	escreve(textoIn);
 	
 	return 0;
 }
 
 /* Lê o texto */
-void leTexto(char vetor[]){
-	int lim = 10; //limite de entrada por linha
+void leTexto(char vetor[], int max){
+	int lim = 100; //limite de entrada por linha
 	int i, j, k=0, c;
 	
 	c=getchar();
@@ -37,10 +42,32 @@ void leTexto(char vetor[]){
 }
 
 void escreve(char vetor[]){
-	int i, max = 8;
+	int i, max = 80;
 	for(i=0;vetor[i]!='\0';i++){
 		if((i+1)%max == 0) printf("\n");
 		printf("%c",vetor[i]);
 	}
 	printf("\n");
+}
+
+void criptografa(char textoIn[], int vet_num[]){
+	int i;
+	sentido = AVANCA;
+	for(i=0;vetor[i]!='\0';i++){
+		if()
+	}
+}
+
+void maiuscula(char vetor[], int c){
+	char base[] = " ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+}
+
+void minuscula(char vetor[], int c){
+	char base[] = " abcdefghijklmnopqrstuvwxyz";
+}
+
+void avancar(){
+}
+
+void recuar(){
 }
