241528d4593ff4447fc83d75f60f4584601fc675
Ester G. Pais <estergpais@hotmail.com>
2018-11-29 16:10:20 -0200

    Update P1.c

--- P1.c
+++ P1.c
@@ -1,33 +1,46 @@
 #include <stdio.h>
 
+void leTexto(char vetor[]);
+void escreve(char vetor[]);
+void maiuscula(char vetor[]);
+void minuscula(char vetor[]);
 void avancar();
 void recuar();
-void leTexto(char vetor[]);
-void leLinha(char vetor[], int lim);
-void escreve();
 
-void main(){
+int main(){
+	char textoIn[3000];
+	leTexto(textoIn);
+	escreve(textoIn);
 	
+	return 0;
 }
 
-/* Lê o texto chamando a função leLinha
- * a cada linha nova
- */
+/* Lê o texto */
 void leTexto(char vetor[]){
-	int lim = 100;
-	int i;
+	int lim = 10; //limite de entrada por linha
+	int i, j, k=0, c;
 	
-	for(i=0;vetor[i]!=EOF;i++){
-		
+	c=getchar();
+	for(i=0;c!=EOF;i++){
+		if(i==0) vetor[k++] = c;
+		/* Trata cada linha e trunca quem tem mais de 100 caracteres
+		 * Dar uma mensagem de erro ao ultrapassar
+		 */
+		for(j=0;j<lim && (c=getchar()) != EOF && c != '\n'; ++j){
+			if(c == '\n' || c == '\t') c = ' ';
+			vetor[k++] = c;
+		}
+		if(j==lim) while(getchar() != '\n');
+		if(c != EOF) vetor[k++] = ' ';
 	}
+	vetor[k] = '\0';
 }
-/* Trata cada linha e trunca quem tem mais de 100 caracteres
- * Dar uma mensagem de erro ao ultrapassar
- */
-void leLinha(char vetor[], int lim){
-	int c, i;
 
-	for(i=0; i<lim-1&&(c=getchar())!=EOF && c!='\n'; ++i){
-		vet[i] = c;
+void escreve(char vetor[]){
+	int i, max = 8;
+	for(i=0;vetor[i]!='\0';i++){
+		if((i+1)%max == 0) printf("\n");
+		printf("%c",vetor[i]);
 	}
+	printf("\n");
 }
